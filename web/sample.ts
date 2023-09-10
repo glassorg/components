6faff889f709ff65f9b4c1625c8aa1beb64c589b
KJK <kody.j.king@gmail.com>
2023-09-09 20:18:03 -0700

    Fixed compiler hanging.

--- web/sample.ts
+++ web/sample.ts
@@ -1,13 +1,16 @@
 
 import * as svg from "../src/svg/elements.js";
-import { div, script, span } from "../src/html/elements.js";
+import { elements } from "../src/html/elements.js";
+
+const { div, span, canvas, script } = elements
 
 function helloWorld() {
     return div(
         span({ style: { color: "blue" } }, "Hello"),
         " ",
         span({ style: { color: "red" } }, "World"),
-        script(`console.log("Logging from my custom script");`)
+        script(`console.log("Logging from my custom script");`),
+        canvas({ width: 100, height: 100 })
     )
 }
 
